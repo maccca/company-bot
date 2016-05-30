9c234383a0c018172496d3949944e4c9e5a9059c
Andrew McIntosh <andrew.mcintosh@promisepay.com>
2016-05-30 18:26:20 +1000

    - Worked on async summary of all tickets, calculates at last user - Broke out the messaging functions - Summary allows a breakdown without pinging everyone

--- scripts/script.js
+++ scripts/script.js
@@ -8,98 +8,124 @@ var zendeskDomain = process.env.ZENDESK_DOMAIN;
 var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
 
 module.exports = function(robot) {
-  robot.respond(/hi|hello|help/i, function(res) {
-      res.reply("Greetings! I am here to help Czech that we are helping our lovely customers.?\n`checkin` - Check in with all the Specialists.");
-  });
+    robot.respond(/hi|hello|help/i, function(res) {
+        console.log(res);
+        res.reply("Greetings! Let me help you!\n`czech` - Check in with all the Specialists.\n`report` - Report all tickets for Specialists.");
+    });
 
-  robot.respond(/czechin|checkin|check/i, function(res) {
-    res.reply('Messaging a break-down of ZenDesk tickets to each of the Integration Support specialists.');
+    robot.respond(/report/i, function(res) {
+        console.log(res);
+        res.reply('Messaging a report of all ZenDesk tickets.');
+        report(res, false);
+    });
 
-    var users = [{
-        name: process.env.USER1_NAME,
-        zID: process.env.USER1_ZENDESK_ID,
-        sID: process.env.USER1_SLACK_ID,
-        zendeskView: process.env.USER1_ZENDESK_VIEW,
-        zendeskTag: process.env.USER1_ZENDESK_TAG
-    }, {
-        name: process.env.USER2_NAME,
-        zID: process.env.USER2_ZENDESK_ID,
-        sID: process.env.USER2_SLACK_ID,
-        zendeskView: process.env.USER2_ZENDESK_VIEW,
-        zendeskTag: process.env.USER2_ZENDESK_TAG
-    }, {
-        name: process.env.USER3_NAME,
-        zID: process.env.USER3_ZENDESK_ID,
-        sID: process.env.USER3_SLACK_ID,
-        zendeskView: process.env.USER3_ZENDESK_VIEW,
-        zendeskTag: process.env.USER3_ZENDESK_TAG
-    }, {
-        name: process.env.USER4_NAME,
-        zID: process.env.USER4_ZENDESK_ID,
-        sID: process.env.USER4_SLACK_ID,
-        zendeskView: process.env.USER4_ZENDESK_VIEW,
-        zendeskTag: process.env.USER4_ZENDESK_TAG
-    }, {
-        name: process.env.USER5_NAME,
-        zID: process.env.USER5_ZENDESK_ID,
-        sID: process.env.USER5_SLACK_ID,
-        zendeskView: process.env.USER5_ZENDESK_VIEW,
-        zendeskTag: process.env.USER5_ZENDESK_TAG
-    }, {
-        name: process.env.USER6_NAME,
-        zID: process.env.USER6_ZENDESK_ID,
-        sID: process.env.USER6_SLACK_ID,
-        zendeskView: process.env.USER6_ZENDESK_VIEW,
-        zendeskTag: process.env.USER6_ZENDESK_TAG
-    }];
+    robot.respond(/czechin|czech|checkin|check/i, function(res) {
+        console.log(res);
+        res.reply('Messaging a break-down of ZenDesk tickets to each of the Integration Support specialists.');
+        report(res, true);
+    });
 
-    var summary = '\n*Summary:*\n';
+    function messageUser(user, message) {
+        console.log(message);
+        robot.adapter.customMessage({
+            channel: user,
+            attachments: [{
+                title_link: 'Summary',
+                fallback: message,
+                text: message,
+                mrkdwn_in: ['text']
+            }]
+        });
+    };
 
-    async.each(users, function(user, done) {
-      var url = 'https://' + zendeskDomain + '.zendesk.com/api/v2/search.json?query=type:ticket status:open tags:' + user.zendeskTag;
+    function messageSummary(res, summary, totalCount, totalHours) {
+        var message = '\n :clipboard: *Summary* `' + totalCount + '` tickets `' + Math.floor(totalHours / totalCount) + '` hrs average\n\n' + summary;
+        console.log(message);
+        res.reply(message);
+    };
 
-          robot
-            .http(url)
-            .header('Authorization', auth)
-            .get(params)(function(err, response, body) {
+    function report(res, notifyUser) {
+        var users = [{
+            name: process.env.USER1_NAME,
+            zID: process.env.USER1_ZENDESK_ID,
+            sID: process.env.USER1_SLACK_ID,
+            zendeskView: process.env.USER1_ZENDESK_VIEW,
+            zendeskTag: process.env.USER1_ZENDESK_TAG
+        }, {
+            name: process.env.USER2_NAME,
+            zID: process.env.USER2_ZENDESK_ID,
+            sID: process.env.USER2_SLACK_ID,
+            zendeskView: process.env.USER2_ZENDESK_VIEW,
+            zendeskTag: process.env.USER2_ZENDESK_TAG
+        }, {
+            name: process.env.USER3_NAME,
+            zID: process.env.USER3_ZENDESK_ID,
+            sID: process.env.USER3_SLACK_ID,
+            zendeskView: process.env.USER3_ZENDESK_VIEW,
+            zendeskTag: process.env.USER3_ZENDESK_TAG
+        }, {
+            name: process.env.USER4_NAME,
+            zID: process.env.USER4_ZENDESK_ID,
+            sID: process.env.USER4_SLACK_ID,
+            zendeskView: process.env.USER4_ZENDESK_VIEW,
+            zendeskTag: process.env.USER4_ZENDESK_TAG
+        }, {
+            name: process.env.USER5_NAME,
+            zID: process.env.USER5_ZENDESK_ID,
+            sID: process.env.USER5_SLACK_ID,
+            zendeskView: process.env.USER5_ZENDESK_VIEW,
+            zendeskTag: process.env.USER5_ZENDESK_TAG
+        }, {
+            name: process.env.USER6_NAME,
+            zID: process.env.USER6_ZENDESK_ID,
+            sID: process.env.USER6_SLACK_ID,
+            zendeskView: process.env.USER6_ZENDESK_VIEW,
+            zendeskTag: process.env.USER6_ZENDESK_TAG
+        }];
+
+        var summary = '';
+        var totalCount = 0;
+        var syncedUsers = 0;
+        var totalHours = 0;
+        async.each(users, function(user, done) {
+            var url = 'https://' + zendeskDomain + '.zendesk.com/api/v2/search.json?query=type:ticket status:open tags:' + user.zendeskTag;
+
+            robot.http(url).header('Authorization', auth).get()(function(err, response, body) {
+                syncedUsers += 1;
                 var jsonResponse = JSON.parse(body);
                 var ticketCount = jsonResponse.count;
                 var message;
-
+                totalCount += ticketCount;
                 if (ticketCount > 0) {
                     message = 'Hi ' + user.name + '! There are `' + ticketCount + '` developer support tickets waiting for <https://' + zendeskDomain + '.zendesk.com/agent/filters/' + user.zendeskView + '|you>. :hugging_face:\n';
+                    var count = 1;
+                    jsonResponse.results.forEach(function(ticket, index) {
+                        var ticketUrl = 'https://ppay.zendesk.com/agent/tickets/' + ticket.id;
+                        var ticketSubject = ticket.subject;
+                        var ticketUpdatedAt = ticket.updated_at;
 
-                  jsonResponse.results.forEach(function(ticket, index) {
-                    var ticketUrl = 'https://ppay.zendesk.com/agent/tickets/' + ticket.id;
-                    var ticketSubject = ticket.subject;
-                    var ticketUpdatedAt = ticket.updated_at;
-
-                    var now = new Date().getTime();
-                    var ticketUpdateTime = new Date(ticketUpdatedAt).getTime();
-                    var hoursAgo = Math.floor((now - ticketUpdateTime) / (60*60*1000));
-
-                    message += '<' + ticketUrl + '|' + ticketSubject + '> - :timer_clock: ' + hoursAgo + ' hrs\n';
-                  });
+                        var now = new Date().getTime();
+                        var ticketUpdateTime = new Date(ticketUpdatedAt).getTime();
+                        var hoursAgo = Math.floor((now - ticketUpdateTime) / (60 * 60 * 1000));
+                        totalHours += hoursAgo;
+                        message += count + ') <' + ticketUrl + '|' + ticketSubject + '> - :timer_clock: ' + hoursAgo + ' hrs\n';
+                        count += 1;
+                    });
+                    summary += '*' + user.name + '* - `' + ticketCount + '` outstanding tickets.\n';
                 } else {
-                  // message = 'Hi ' + user.name + '! You have no tickets! :thumbsup:';
+                    summary += user.name + ' - `' + ticketCount + '` outstanding tickets.\n';
+                }
+                if (notifyUser) {
+                    messageUser(user.sID, message);
                 }
 
-                robot.adapter.customMessage({
-                    channel: user.sID,
-                    attachments: [{
-                        title_link: 'Summary',
-                        fallback: message,
-                        text: message,
-                        mrkdwn_in: ['text']
-                    }]
-                });
-
-                summary += '*' + user.name + '* - `' + ticketCount + '` outstanding tickets.\n';
-
-                done();
+                // done();
+                if (syncedUsers == users.length) {
+                    messageSummary(res, summary, totalCount, totalHours);
+                }
             });
-      }, function(err) {
-          res.reply(summary);
-      });
-  });
+        }, function(error) {
+
+        });
+    };
 }
